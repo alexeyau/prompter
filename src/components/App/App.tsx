f02a1358bc3adb45cfa2e0cdcf786305bfbd028d
Alexey Au <alexeyau@>
2023-10-14 12:18:43 +0400

    add tailwind

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+
+import reactLogo from '../../assets/react.svg'
 import viteLogo from '/vite.svg'
+import Editor from '../Editor/Editor.tsx'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -16,19 +16,10 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
 
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
 
       <div className="container">
         <div className="column">
@@ -43,6 +34,7 @@ function App() {
         </div>
         <div className="column">
             <div className="post-content">Содержание записи ла-ла-ла.</div>
+            <Editor />
         </div>
     </div>
     </>
---
+++ src/components/Editor/Editor.tsx
@@ -0,0 +1,15 @@
+import './Editor.css'
+
+function Editor() {
+
+  return (
+    <div className="Editor">
+      <textarea>
+        La la la prompt
+      </textarea>
+      <button>Save</button>
+    </div>
+  )
+}
+
+export default Editor
